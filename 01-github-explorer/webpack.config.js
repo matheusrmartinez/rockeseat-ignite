53b396c494fc53f8d1d211f25589dd8c42c6652b
Matheus Martinez <matheus.rmartinez@gmail.com>
2021-05-28 08:00:52 -0300

    Criação de RepositoryList

--- 01-github-explorer/webpack.config.js
+++ 01-github-explorer/webpack.config.js
@@ -20,15 +20,15 @@ module.exports = {
     plugins: [ new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'public', 'index.html')}) ],
     module: {
         rules: [
-            {
+            { 
                 test: /\.jsx$/,
                 exclude: /node_modules/,
                 use: 'babel-loader'
             },
             {
-                test: /\.css$/,
+                test: /\.scss$/,
                 exclude: /node_modules/,
-                use: ['style-loader', 'css-loader']
+                use: ['style-loader', 'css-loader', 'sass-loader']
             },
         ]
     }
