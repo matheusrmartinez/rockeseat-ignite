66bd534dd6104e2d0c734e52e2ae1a8e05b76b65
Matheus Martinez <matheus.rmartinez@gmail.com>
2021-06-07 08:00:06 -0300

    Renomeção de arquivos para typescript

--- 01-github-explorer/src/components/RepositoryList.tsx
+++ 01-github-explorer/src/components/RepositoryList.tsx
@@ -2,8 +2,15 @@ import { RepositoryItem } from "./RepositoryItem";
 import '../styles/repositories.scss';
 import { useEffect, useState } from "react";
 
+type Repository = {
+name: string;
+description: string;
+html_url: string;
+}
+
+
 export function RepositoryList(){
-    const[repositories, setRepositories] = useState([]);
+    const[repositories, setRepositories] = useState<Repository[]>([] as Repository[]);
 
     useEffect(() => {
         fetch('https://api.github.com/orgs/rocketseat/repos')
